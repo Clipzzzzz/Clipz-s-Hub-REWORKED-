f5354b7d26bd0ad425a3dd1103c901237b572389
Clipzzzzz <125373144+Clipzzzzz@users.noreply.github.com>
2023-06-25 21:53:43 -0500

    Update LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -1,12 +1,11 @@
-local project = "https://raw.githubusercontent.com/Clipzzzzz/Project-Clipz/main/"
-
 local games = {
-	[11345129632] = "Roll-the-Dice.lua"
+	[1] = {11345129632, "🎲ROLL The DICE!"}
 }
 
-for i, v in pairs(games) do
-	if i == game.PlaceId or i == game.GameId then
-		 loadstring(game:HttpGet(project .. v))()
+for i,v in games do
+	if game.PlaceId == v[1] then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Clipzzzzz/Project-Clipz/main/"..v[2], true))()
+		print("loadstring success")
 	end
 end
 
