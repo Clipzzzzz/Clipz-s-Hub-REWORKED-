dca93d2eeccbaef24ddc7593591ca82a6672fbd7
Clipz <125373144+Clipzzzzz@users.noreply.github.com>
2023-07-06 11:14:25 -0500

    Update LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -1,6 +1,6 @@
 local games = {
 	[1] = {11345129632, "🎲ROLL%20The%20DICE!", "🎲ROLL The DICE!"}
-	[2] = {9216815133, "Trollge%20Conventions%20[GT%20GASTER]", "Trollge Conventions [GT GASTER]"}
+	[2] = {9216815133, "Trollge%20Conventions%20%5BGT%20GASTER%5D", "Trollge Conventions [GT GASTER]"}
 }
 
 for i,v in games do
