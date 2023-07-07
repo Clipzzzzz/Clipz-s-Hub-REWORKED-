783c0e76a9f37d4a1ecee9121009f2ce3d6aa574
Clipz <125373144+Clipzzzzz@users.noreply.github.com>
2023-07-07 14:16:07 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,4 +1,4 @@
 local PCLIB = loadstring(game:HttpGet('https://raw.githubusercontent.com/Clipzzzzz/Project-Clipz/main/source.lua'))()
 
-local Menu = PCLIB.NameLib("TEST")
-local Tab = Menu:AddTab("Aky sucks 1")
+local Menu = PCLIB:NameLib("TEST")
+local Tab = PCLIB:AddTab("Aky sucks 1")
